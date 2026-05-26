6c1b90ac45c60de26ec7dd07aaf7e02074e7d900
alexomelc <github.outmost351@passmail.com>
2026-05-26 19:09:16 -0400

    add search

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -3,48 +3,37 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const path = require('path');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
-// Items API Task 1: Импортируем роуты
-const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 const app = express();
-app.use("*", cors());
+app.use("*",cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
-.catch((e) => console.error('Failed to connect to DB', e));
+    .catch((e) => console.error('Failed to connect to DB', e));
+
 
 app.use(express.json());
 
 // Route files
-// authRoutes Step 2: import the authRoutes
-//{{insert code here}}
-
-// Search API Task 1: import the searchRoutes
-//{{insert code here}}
-
-
+const secondChanceRoutes = require('./routes/secondChanceItemsRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use Routes
-// authRoutes Step 2: add the authRoutes
-//{{insert code here}}
-
-// Items API Task 2: Подключаем роуты к серверу
-app.use('/api/secondchance/items', secondChanceItemsRoutes);
-
-// Search API Task 2: add the searchRoutes
-//{{insert code here}}
-
+app.use('/api/secondchance/items', secondChanceRoutes);
+app.use('/api/secondchance/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
@@ -52,9 +41,9 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/", (req, res) => {
-    res.send("Inside the server");
-});
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
